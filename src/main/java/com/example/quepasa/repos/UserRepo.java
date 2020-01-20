af9ac8e210af90878b938d3fd6ba9fa212a21ae9
Kirill Perevozov <45182370+K33RL@users.noreply.github.com>
2020-01-20 05:15:40 +0300

    Added : registration and authorization dependencies between databases tables(OneToMany)

---
+++ src/main/java/com/example/quepasa/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.quepasa.repos;
+
+import com.example.quepasa.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+}
