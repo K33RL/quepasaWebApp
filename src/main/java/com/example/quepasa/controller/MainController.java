bfc4262c27c93d33b66020dd025120fb490900f0
Kirill Perevozov <45182370+K33RL@users.noreply.github.com>
2020-01-25 22:56:42 +0300

    Added: ManyToMany , subscriptions

--- src/main/java/com/example/quepasa/controller/MainController.java
+++ src/main/java/com/example/quepasa/controller/MainController.java
@@ -106,6 +106,10 @@ public class MainController {
     ){
         Set<Message> messages = user.getMessages();
 
+        model.addAttribute("userChannel", user);
+        model.addAttribute("subscriptionsCount", user.getSubscriptions().size());
+        model.addAttribute("subscribersCount", user.getSubscribers().size());
+        model.addAttribute("isSubscriber", user.getSubscribers().contains(currentUser));
         model.addAttribute("messages", messages);
         model.addAttribute("message", message);
         model.addAttribute("isCurrentUser",currentUser.equals(user));
@@ -136,4 +140,5 @@ public class MainController {
 
         return "redirect:/user-messages/" + user.getId();
     }
+
 }
